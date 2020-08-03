e2507c6f36a21231b24b09f38088521ae985430b
Damara G <damarasg@gmail.com>
2020-08-03 13:21:23 -0400

    changed testing code in fold_sim

--- fold_simultaneous.py
+++ fold_simultaneous.py
@@ -30,32 +30,33 @@ def sim_fold_names(graph):
 if __name__ == "__main__":
     import file_IO_helper as fio
     import strongly_connected_components as scc
+    folder = "states_from_tri_and_ic"
 
-    graph = fio.read_graph(2)
-    node = graph.nodes[3]
-    node.head[0][2] = False
-    node = graph.nodes[1]
-    node.head[0][2] = False
-    print(sim_fold_names(graph)) # should not be empty!
+##    graph = fio.read_graph(2)
+##    node = graph.nodes[3]
+##    node.head[0][2] = False
+##    node = graph.nodes[1]
+##    node.head[0][2] = False
+##    print(sim_fold_names(graph)) # should not be empty!
 
     print("Let's try it on our automaton:")
 
-    part = scc.partition_states()
+    part = scc.partition_states(folder)
     #want to look at the second scc of 5 edge states
     print("five edges:")
-    comps = scc.partition_part_scc(part[5])[1]
+    comps = scc.partition_part_scc(part[5], folder)[2]
     for state in comps:
-        print(sim_fold_names(fio.read_graph(state)))
+        print(sim_fold_names(fio.read_graph(state, folder)))
 
     print("four edges:")
-    comps = scc.partition_part_scc(part[4])
+    comps = scc.partition_part_scc(part[4], folder)
     for comp in comps:
         for state in comp:
             print(state)
-            print(sim_fold_names(fio.read_graph(state)))
+            print(sim_fold_names(fio.read_graph(state, folder)))
 
     print("three edges:")
-    comps = scc.partition_part_scc(part[3])
+    comps = scc.partition_part_scc(part[3], folder)
     for comp in comps:
         for state in comp:
-            print(sim_fold_names(fio.read_graph(state)))
+            print(sim_fold_names(fio.read_graph(state, folder)))
